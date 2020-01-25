f4f192718001d5732ae060da4c64fc037c5c4e6b
Christian Freitas <chrfreitas@gmail.com>
2020-01-25 09:20:06 -0300

    Add router to app

--- src/App.svelte
+++ src/App.svelte
@@ -1,42 +1,14 @@
 <script>
-  import { goals } from "./App.store.js";
-
-  let goal = {
-    name: null,
-    value: null,
-    data: null
-  };
-
-  const clearForm = () => {
-    goal = { name: null, value: null, data: null };
-  };
-
-  const handleOnSubmit = () => {
-    goals.add(goal);
-    clearForm();
-  };
+  import { Router, Link, Route } from "svelte-routing";
+  import Goals from "./Goals.svelte";
 </script>
 
-<form on:submit|preventDefault={handleOnSubmit}>
-  <label for="">Name</label>
-  <input type="text" bind:value={goal.name} />
-
-  <label for="">Valor</label>
-  <input type="text" bind:value={goal.value} />
-
-  <label for="">Data</label>
-  <input type="text" bind:value={goal.data} />
-
-  <button>Save</button>
-</form>
-
-<ul>
-  {#each $goals as goal}
-    <li>
-      <div>
-        <span>Name: {goal.name}</span>
-        <span on:click={() => goals.delete(goal.name)}>X</span>
-      </div>
-    </li>
-  {/each}
-</ul>
+<Router>
+  <nav>
+    <Link to="/goals">Add goal</Link>
+    <Link to="/#">Add investment</Link>
+  </nav>
+  <div>
+    <Route path="/goals" component={Goals} />
+  </div>
+</Router>
---
+++ src/Goals.svelte
@@ -0,0 +1,42 @@
+<script>
+  import { goals } from "./store";
+
+  let goal = {
+    name: null,
+    value: null,
+    data: null
+  };
+
+  const clearForm = () => {
+    goal = { name: null, value: null, data: null };
+  };
+
+  const handleOnSubmit = () => {
+    goals.add(goal);
+    clearForm();
+  };
+</script>
+
+<form on:submit|preventDefault={handleOnSubmit}>
+  <label for="">Name</label>
+  <input type="text" bind:value={goal.name} />
+
+  <label for="">Valor</label>
+  <input type="text" bind:value={goal.value} />
+
+  <label for="">Data</label>
+  <input type="text" bind:value={goal.data} />
+
+  <button>Save</button>
+</form>
+
+<ul>
+  {#each $goals as goal}
+    <li>
+      <div>
+        <span>Name: {goal.name}</span>
+        <a href="?deleted" on:click={() => goals.delete(goal.name)}>Delete</a>
+      </div>
+    </li>
+  {/each}
+</ul>
